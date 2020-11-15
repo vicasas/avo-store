9c763a4db0bf316ba691a04f2b8bea84a011b8db
Victor Casas <victor.casas@outlook.cl>
2020-11-15 11:21:13 -0300

    feat: detail product name

---
+++ pages/product/[id].js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+import { useRouter } from 'next/router'
+
+export default function Home() {
+  const router = useRouter()
+  const [product, setProduct] = useState({})
+
+  const param = router.query?.id
+
+  useEffect(() => {
+    if (param !== undefined) {
+      window
+        .fetch(`/api/avo/${router.query?.id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setProduct(data)
+        })
+    }
+  }, [])
+
+  return (
+    <div>
+      <ul>
+        <li>{product.name}</li>
+      </ul>
+    </div>
+  )
+}
