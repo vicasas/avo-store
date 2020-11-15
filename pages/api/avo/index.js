94b392bc7e7cf2c056e7149024db85c562e85a52
Victor Casas <victor.casas@outlook.cl>
2020-11-15 11:19:48 -0300

    refactor: api allAvos

--- pages/api/avo/index.js
+++ pages/api/avo/index.js
@@ -2,25 +2,17 @@ import DB from '../../../db/'
 
 const allAvos = async (req, res) => {
   try {
-    console.log('init')
     const db = new DB()
     const allEntries = await db.getAll()
     const lenght = allEntries.length
 
-    console.log(allEntries)
-
     // Notice: We're manually setting the response object
     // However Next.JS offers Express-like helpers :)
     // https://nextjs.org/docs/api-routes/response-helpers
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ lenght, data: allEntries }))
+    res.status(200).json({ lenght, data: allEntries })
   } catch (e) {
     console.error(e)
-    res.statusCode = 500
-    res.end(
-      JSON.stringify({ length: 0, data: [], error: 'Something went wrong' })
-    )
+    res.status(500).json({ length: 0, data: [], error: 'Something went wrong' })
   }
 }
 
