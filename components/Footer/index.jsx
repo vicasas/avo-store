1324a944ee74b0d22419379a8e20b16000ccdd65
Victor Casas <victor.casas@outlook.cl>
2020-11-15 13:46:52 -0300

    feat: add component Footer

---
+++ components/Footer/index.jsx
@@ -0,0 +1,46 @@
+import Link from 'next/link'
+
+export default function Footer() {
+  return (
+    <footer className='py-4 mt-5 border-top'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-3'>
+            <h4>Nosotros</h4>
+            <Link href='/about'>
+              <a>Conoce más</a>
+            </Link>
+          </div>
+          <div className='col-sm-3'>
+            <h4>Servicios</h4>
+            <Link href='/'>
+              <a>Todos los productos</a>
+            </Link>
+          </div>
+          <div className='col-sm-6'>
+            <h4>Hecho para</h4>
+            <p>La comunidad de desarrolladores JavaScript que usan <a href='https://nextjs.org/'>Next.js</a> con el fin de aprender este magnifico Framework.</p>
+            <div className='d-flex'>
+              <div className='mr-3'>
+                <i className='mr-1'>icon</i>
+                <a href='#!'>Twitter</a>
+              </div>
+              <div>
+                <i className='mr-1'>icon</i>
+                <a href="#!">GitHub</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='mt-5'>
+        <p className='text-center m-0'>
+          <small>Icons made by <a>Freepik</a> from <a>www.flaticon.com</a></small>
+        </p>
+        <p className='text-center m-0'>
+          <small>Avocado images taken from <a>Avocado 101</a> at <a>California Avocado</a></small>
+        </p>
+      </div>
+    </footer>
+  )
+}
