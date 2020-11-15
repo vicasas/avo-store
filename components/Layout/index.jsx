a6077aafa4940745f4cc7cb1ce2cabb59dfc916c
Victor Casas <victor.casas@outlook.cl>
2020-11-15 13:47:09 -0300

    feat: use component Footer

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,11 +1,12 @@
 import Navbar from "@components/Navbar"
+import Footer from "@components/Footer"
 
 export default function Layout({ children }) {
   return (
     <div>
       <Navbar />
       {children}
-      <footer>This is the footer</footer>
+      <Footer />
     </div>
   )
 }
