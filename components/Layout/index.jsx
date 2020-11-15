163d2ca4fb8ad2d508ed0f49a33c1f222c981cfe
Victor Casas <victor.casas@outlook.cl>
2020-11-15 18:48:30 -0300

    feat: add main tag html

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -7,7 +7,9 @@ export default function Layout({ children }) {
       <header>
         <Navbar />
       </header>
-      {children}
+      <main>
+        {children}
+      </main>
       <Footer />
     </div>
   )
