6f920da22e6a4f16d62c822b9de6e4e408a4250d
Victor Casas <victor.casas@outlook.cl>
2020-11-15 11:38:29 -0300

    feat: extend document

---
+++ pages/_document.jsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  // Use only if you need to do something from the server
+  // static async getInitialProps(ctx) {
+  //   const initialProps = await Document.getInitialProps(ctx)
+  //   return { ...initialProps }
+  // }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
