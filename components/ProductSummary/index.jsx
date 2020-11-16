3544b41e4f9b4f08ca156bb87a558c5c6e1ce37d
Victor Casas <victor.casas@outlook.cl>
2020-11-16 19:36:30 -0300

    feat: add styles

--- components/ProductSummary/index.jsx
+++ components/ProductSummary/index.jsx
@@ -1,4 +1,5 @@
 import AddToCart from "@components/AddToCart";
+import styles from './product-summary.module.css'
 
 export default function ProductSummary({ product }) {
   return (
@@ -9,9 +10,9 @@ export default function ProductSummary({ product }) {
       <div className='col-sm-7'>
         <h1 className='h4'>{product.name}</h1>
         <div>
-          <div>{product.price}</div>
-          <div className='py-1 text-uppercase d-inline-block'>
-            <span>{`sku: ${product.sku}`}</span>
+          <div className='mb-1'>{product.price}</div>
+          <div className='py-1 mb-3 text-uppercase'>
+            <span className={styles.chip}>{`sku: ${product.sku}`}</span>
           </div>
         </div>
         <AddToCart />
