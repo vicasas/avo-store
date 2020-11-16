9c41380652d2aa154e75596e43f3cf83656a1ce9
Victor Casas <victor.casas@outlook.cl>
2020-11-16 08:56:53 -0300

    feat: add Link to detail product

--- components/ProductList/index.jsx
+++ components/ProductList/index.jsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function ProductList({ products, loading }) {
   return (
     <div className='container'>
@@ -5,22 +7,24 @@ export default function ProductList({ products, loading }) {
         {!loading ? (
           products.map((product) => (
             <div key={product.id} className='col-sm-6 col-md-4'>
-              <div className='card mb-5'>
-                <img src={product.image} className='card-img-top' alt='' />
-                <div className="card-body border-top">
-                  <h5 className='card-title'>{product.name}</h5>
-                  <p className='card-text'>{product.price}</p>
+              <Link href="/product/[id]" as={`/product/${product.id}`} passHref>
+                <div className='card mb-5'>
+                  <img src={product.image} className='card-img-top img-fluid' alt='' title='' width={340} height={340} />
+                  <div className="card-body border-top">
+                    <h5 className='card-title'>{product.name}</h5>
+                    <p className='card-text'>{product.price}</p>
+                  </div>
                 </div>
-              </div>
+              </Link>
             </div>
           ))
         ) : (
-          [0, 1, 2].map((num) => (
-            <div key={num} className='col-sm-6 col-md-4'>
-              loading...
-            </div>
-          ))
-        )}
+            [0, 1, 2].map((num) => (
+              <div key={num} className='col-sm-6 col-md-4'>
+                loading...
+              </div>
+            ))
+          )}
       </div>
     </div>
   )
