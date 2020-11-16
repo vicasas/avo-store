b657d5466779f93b62b218292efd493830447290
Victor Casas <victor.casas@outlook.cl>
2020-11-16 19:35:50 -0300

    feat: add new design AddToCard component

--- components/AddToCart/index.jsx
+++ components/AddToCart/index.jsx
@@ -1,8 +1,8 @@
 export default function AddToCart() {
   return (
-    <div>
-      <input type='number' />
-      <button>button</button>
+    <div className="input-group mb-3">
+      <input type="number" className="form-control" placeholder="Quantity" aria-label="Recipient's username" aria-describedby="button-addon2" />
+      <button className="btn btn-primary" type="button" id="button-addon2">Add to Cart</button>
     </div>
   )
 }
