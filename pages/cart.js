b9a8d254bcc38fc045ff2ee19ddb7b90bc25d338
Victor Casas <victor.casas@outlook.cl>
2020-11-16 19:49:53 -0300

    feat: add page cart

---
+++ pages/cart.js
@@ -0,0 +1,10 @@
+export default function Cart() {
+  return (
+    <div className='container'>
+      <div class="alert alert-warning" role="alert">
+        <h4 class="alert-heading">Your cart is empty</h4>
+        <p>You will need to add some items to the cart before you can checkout.</p>
+      </div>
+    </div>
+  )
+}
