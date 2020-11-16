fd0dafe67dbc179774620a3306c63ef571895f1a
Victor Casas <victor.casas@outlook.cl>
2020-11-16 19:49:37 -0300

    feat: add title to page about

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 export default function About() {
   return (
-    <div>
-      <div>This is page about</div>
+    <div className='container'>
+      <h1 className='h3 text-center mb-3'>13 Surprising Facts About Avocados</h1>
     </div>
   )
 }
