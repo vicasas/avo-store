55e64875fbef45d4d163473c70d4455a5640a3c5
Victor Casas <victor.casas@outlook.cl>
2020-11-17 20:00:58 -0300

    refactor: rename extension files

--- components/ProductSummary/index.jsx
+++ components/ProductSummary/index.jsx
@@ -1,5 +1,4 @@
-import AddToCart from "@components/AddToCart";
-import styles from './product-summary.module.css'
+import AddToCart from "@components/AddToCart"
 
 export default function ProductSummary({ product }) {
   return (
@@ -12,11 +11,23 @@ export default function ProductSummary({ product }) {
         <div>
           <div className='mb-1'>{product.price}</div>
           <div className='py-1 mb-3 text-uppercase'>
-            <span className={styles.chip}>{`sku: ${product.sku}`}</span>
+            <span className='chip'>{`sku: ${product.sku}`}</span>
           </div>
         </div>
         <AddToCart />
       </div>
+      <style jsx>{`
+        .chip {
+          vertical-align: baseline;
+          margin: 0 .14285714em;
+          background-color: #e8e8e8;
+          padding: .3833em .433em;
+          color: rgba(0,0,0,.6);
+          text-transform: uppercase;
+          font-size: .75714286rem;
+          border-radius: .28571429rem;
+        }
+      `}</style>
     </section>
   )
 }
